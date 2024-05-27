b4f57e0f3b7a32e8d056484983afdeac866d40f1
AiswaryaAjayakumar <aiswaryaajayakumar003@gmail.com>
2024-05-27 15:19:40 +0530

    updated

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -27,6 +27,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final homeState = Provider.of<HomeScreenController>(context);
+    // final imgUrl = "https://mansharcart.com/image/";
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.grey,
@@ -48,15 +49,16 @@ class _HomeScreenState extends State<HomeScreen> {
                 height: MediaQuery.sizeOf(context).height / 3,
                 width: MediaQuery.sizeOf(context).width,
                 decoration: BoxDecoration(
+                    boxShadow: [BoxShadow(blurRadius: 6, color: Colors.grey)],
                     borderRadius: BorderRadius.circular(20),
                     image: DecorationImage(
                         image: NetworkImage(
-                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg4HG-yxvlbLz9VMusTumV-30QWPBZ2Ry_JQ&s"),
+                            "https://mansharcart.com/image/cache/catalog/0__1/MYFB_Al-Mousoohathu-Al-Yoosafiya-Fi-Bayaan-Adillat-Al-Soofiyat_back-copy-scaled-250x250.jpg"),
                         fit: BoxFit.cover),
                     color: Colors.grey),
               ),
               SizedBox(
-                height: 15,
+                height: 40,
               ),
               Text(
                 homeState.product?.id ?? "",
